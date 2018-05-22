96ae8f2aae1d7fae7a242953b734a2da9135bee1
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-22 08:27:44 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,4 +7,11 @@ def my_all?(collection)
   i+=1
 end
 end
-my_all?([]) {|i| i < 4}
+def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+end
